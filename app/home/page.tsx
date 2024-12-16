34607ee27dd2064005ef67ee75e258e04b66ba69
MRCDEV <126817362+marcossdev04@users.noreply.github.com>
2024-12-16 18:05:42 -0300

    advances

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,53 @@
 export default function Home() {
   return (
     <div className="w-full px-3">
-      <div></div>
+      <div className="flex flex-col py-2">
+        <div className="flex px-3 bg-[#5D5D5D] items-center bg-opacity-40 rounded-t-lg text-xs py-1">
+          <div className="flex w-[36%] text-[11px] gap-1 text-zinc-300">
+            <div>16/12</div>
+            <div>21:00</div>
+          </div>
+          <div>Chanpeons League</div>
+        </div>
+        <div className="bg-[#202120] gap-1 px-3 py-1 flex flex-col rounded-b-lg">
+          <div className="flex flex-col text-sm">
+            <div className="flex justify-between items-center">
+              <div>Real Madrid</div>
+              <div>2</div>
+            </div>
+            <div className="flex justify-between items-center">
+              <div>Barcelona</div>
+              <div>1</div>
+            </div>
+          </div>
+          <div className="flex justify-between gap-2 text-xs pb-1">
+            <div className="flex flex-col w-full items-center">
+              <div className="bg-default w-full text-center rounded-t-lg bg-opacity-40 px-3">
+                Goal Line
+              </div>
+              <div className="bg-[#333] rounded-b-lg w-full text-center text-zinc-100">
+                Over 5.0
+              </div>
+            </div>
+            <div className="flex w-full flex-col items-center">
+              <div className="bg-default w-full text-center rounded-t-lg bg-opacity-40 px-3">
+                Odd
+              </div>
+              <div className="bg-[#333] rounded-b-lg w-full text-center text-zinc-100">
+                1.9
+              </div>
+            </div>
+            <div className="flex w-full flex-col items-center">
+              <div className="bg-default w-full text-center rounded-t-lg bg-opacity-40 px-3">
+                Result
+              </div>
+              <div className="bg-[#333] rounded-b-lg w-full text-center text-default">
+                Win
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+import teste from '@/assets/GettyImages-1708473303-e1696338357503.webp'
+
 export default function News() {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="flex flex-col px-5 py-3">
+      <div className="flex bg-[#272927] justify-between p-2 rounded-lg">
+        <div className="flex flex-col gap-2">
+          <div className="text-sm">ESPN</div>
+          <div className="text-xs">
+            Lamine yamall will be transfer to Real Madrid in secound semestrer
+            of 2025
+          </div>
+          <div className="text-xs text-zinc-400">December 16, 2024</div>
+        </div>
+        <div className="w-[150px]">
+          <Image className="rounded-lg border" src={teste} alt="teste" />
+        </div>
+      </div>
     </div>
   )
 }
